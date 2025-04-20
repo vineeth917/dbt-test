7773b996dee816404f7525b353282db36a9845ed
Vineeth Rayadurgam <vineeth.rayadurgam@gmail.com>
2025-04-20 19:02:27 +0000

    updated ma_rsi

--- models/analytics/ma_rsi.sql
+++ models/analytics/ma_rsi.sql
@@ -1,18 +1,14 @@
--- models/analytics/ma_rsi.sql
 {{ config(materialized='table') }}
 
 with base as (
-
   select
     date,
     symbol,
     close
   from {{ ref('stg_stock_prices') }}
-
 ),
 
 ma as (
-
   select
     date,
     symbol,
@@ -28,45 +24,40 @@ ma as (
       rows between 49 preceding and current row
     ) as ma_50
   from base
-
 ),
 
 rsi_calc as (
-
   select
     date,
     symbol,
     close,
     ma_20,
     ma_50,
-    -- calculate gains & losses
     close - lag(close) over (partition by symbol order by date) as change,
     greatest(close - lag(close) over (partition by symbol order by date), 0) as gain,
     greatest(lag(close) over (partition by symbol order by date) - close, 0) as loss
   from ma
-
 ),
 
 rsi as (
-
   select
     date,
     symbol,
     ma_20,
     ma_50,
-    round(100 - (100 / (1 + (
-      sum(gain) over (
-        partition by symbol
-        order by date
-        rows between 13 preceding and current row
-      ) / nullif(sum(loss) over (
-        partition by symbol
-        order by date
-        rows between 13 preceding and current row
-      ), 0)
-    ))), 2) as rsi_14
+    round(
+      100 - (100 / (
+        1 + (
+          sum(gain) over (
+            partition by symbol order by date rows between 13 preceding and current row
+          ) / nullif(sum(loss) over (
+            partition by symbol order by date rows between 13 preceding and current row
+          ), 0)
+        )
+      )),
+      2
+    ) as rsi_14
   from rsi_calc
-
 )
 
 select
